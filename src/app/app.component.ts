ff3915809228368c28ae0be5f61dfeab818c1d89
tamazyanarsen <tamazyanarsen@mail.ru>
2020-06-23 15:59:54 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from './services/request.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testFayrix';
+export class AppComponent implements OnInit {
+
+    constructor(private requestService: RequestService) {
+    }
+
+    columns: any[];
+    data: any[];
+
+    getData(): void {
+        this.requestService.getData().then((_: any) => {
+            const data = _.result;
+            const res = new Set();
+            data.forEach(e => Object.keys(e).forEach(k => res.add(k)));
+            this.columns = Array(...res.values()).filter(e => !String(e).toLowerCase().includes('id'));
+            this.data = data;
+            console.log(this.columns);
+            console.log(data);
+        }).catch(console.warn);
+    }
+
+    changeModel(event) {
+        console.log(event);
+        console.log(this.data);
+        // this.data данные обновленые, можем их сохранить
+    }
+
+    trackByRow(value) {
+        return (Math.random() * 1000).toString();
+    }
+
+    ngOnInit(): void {
+        this.getData();
+    }
+
+    trackByFn(index, item) {
+        return index;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RequestService } from './services/request.service';
+import { ApiInterceptor } from './api.interceptor';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule
+    ],
+    providers: [
+        RequestService, {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ApiInterceptor,
+            multi: true
+        }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
